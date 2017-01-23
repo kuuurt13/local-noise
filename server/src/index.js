933798dfcff43c939448b577981f06645a2c5f02
kuuurt13 <kstac13@gmail.com>
2017-01-22 21:46:06 -0500

    task(server): Added Access-Control middleware

--- server/src/index.js
+++ server/src/index.js
@@ -13,6 +13,15 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  next();
+});
+
 /* Register Routes */
 moduleRoutes.forEach(ctrl => {
   const url = [appConfig.apiUrl, ctrl.url].join('');
