b4c20385cb531fe41ad392af5743cbd3e304a058
kuuurt13 <kstac13@gmail.com>
2017-01-27 06:42:45 -0500

    task(server): Removed default artist image endpont

--- server/src/modules/songkick/controllers/artists.js
+++ server/src/modules/songkick/controllers/artists.js
@@ -5,14 +5,9 @@ const router = express.Router();
 
 const { artistImgSrc, imageSizes } = skConfig;
 
-router.get('/artists/image', defaultImage);
 router.get('/artists/:id/image', redirectToArtistImage);
 
 /* Routes */
-async function defaultImage(req, res) {
-  res.status(200).json({ default: 'Need default image' });
-}
-
 async function redirectToArtistImage(req, res) {
   const { id } = req.params;
   let { size } = req.query;
