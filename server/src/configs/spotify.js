8fe674e0f2d4a3c4560897a639bc28a4eb1180d1
kuuurt13 <kstac13@gmail.com>
2017-05-19 11:57:43 -0400

    task(api): Moved spotify keys to env & generated new keys

--- server/src/configs/spotify.js
+++ server/src/configs/spotify.js
@@ -4,8 +4,8 @@ export default {
   tokenUrl: 'https://accounts.spotify.com/api/token',
   userUrl: 'https://api.spotify.com/v1/me',
   auth: {
-    id: '295dcdb3d10e48449baad39cbbd07fe8',
-    secret: '55796b5c34264d3abe57045b665d70eb',
+    id: process.env.SPOTIFY_CLIENT_ID,
+    secret: process.env.SPOTIFY_SECRET_KEY,
     redirectUrl: process.env.SPOTIFY_REDIRECT_URL,
     scopes: 'user-read-private playlist-modify-public'
   },
